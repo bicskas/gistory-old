217866e27cc6fc6043de20d251d16f8a59881f08
BicskS <bicskas94@gmail.com>
2016-11-09 15:43:50 +0100

    strslug kivéve

--- app/Http/Controllers/NetworkController.php
+++ app/Http/Controllers/NetworkController.php
@@ -44,7 +44,7 @@ class NetworkController extends Controller
 
 		foreach ($nodes as $n) {
 			$targets = [];
-			$force['nodes'][] = ['id' => str_slug($n->nev), 'group' => 1];
+			$force['nodes'][] = ['id' => $n->nev, 'group' => 1];
 
 
 
@@ -61,7 +61,7 @@ class NetworkController extends Controller
 
 		}
 		foreach ($subproject->edge as $e) {
-			$force['links'][] = ['source' => str_slug($e->node1->nev), 'target' => str_slug($e->node2->nev), 'value' => 1];
+			$force['links'][] = ['source' => $e->node1->nev, 'target' => $e->node2->nev, 'value' => 1];
 		}
 
 //		dd(count($force['nodes']),count($force['links']));
